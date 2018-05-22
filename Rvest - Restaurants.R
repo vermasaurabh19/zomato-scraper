a528309073cae315c43142c51b3d8ea842785c1e
vermasaurabh19 <36915967+vermasaurabh19@users.noreply.github.com>
2018-05-22 19:38:57 +0530

    Update Rvest - Restaurants.R

--- Rvest - Restaurants.R
+++ Rvest - Restaurants.R
@@ -70,4 +70,4 @@ top_restaurant$votes <- gsub(' votes','',top_restaurant$votes)
 top_restaurant$cost_for_two <- gsub("\u20b9","",top_restaurant$cost_for_two)
 
 #Saving as output
-write.csv(top_restaurant,'C:/Users/vermas22/Desktop/Consulting/Self Training/top restaurant.csv')
+write.csv(top_restaurant,'top restaurant.csv')
